218ccdf227cf58c6ce9e439a7a736069d0aca03b
Atm0s <atm0s@ya.ru>
2022-06-05 22:59:13 +0300

    feat: add search field instead prompt

--- script.js
+++ script.js
@@ -1,21 +1,33 @@
+const searchInput = document.querySelector('.search-field');
+const searchBtn = document.querySelector('.search-button');
 const macNode = document.querySelector('.mac');
-const resultNode = document.querySelector('.result');
-let range = new Range();
+const macVendor = document.querySelector('.mac-ven');
 
-let mac = prompt("Enter MAC in any format:");
-// let mac = '70:4f:57:90:da:d3';
-mac = mac.toLowerCase().split('').filter(x => x != ':' && x != '-' && x != '.').join('').match(/.{1,2}/g).join(':')
-url = 'https://api.macaddress.io/v1?apiKey=at_GPMaRj94JKmEO70f6xNfpdoYU5bvv&output=json&search=' + mac
 async function getData() {
 	const res = await fetch(url);
 	const data = await res.json();
-	// alert(`Your mac is: \n${mac}\n ${data.vendorDetails.companyName}`);
-	// console.log(data);
-	macNode.innerHTML = mac;
-	resultNode.innerHTML = data.vendorDetails.companyName;
-
-	document.getSelection().selectAllChildren(macNode)
-	// document.execCommand("copy");
-	// macNode.innerHTML.select();
+	macVendor.innerHTML = data.vendorDetails.companyName;
 }
-getData();
+
+searchBtn.addEventListener('click', () => {
+	let mac = searchInput.value
+		.toLowerCase()
+		.split('')
+		.filter(x => x != ':' && x != '-' && x != '.')
+		.join('')
+		.match(/.{1,2}/g)
+		.join(':');
+	macNode.innerHTML = mac;
+	url = 'https://api.macaddress.io/v1?apiKey=at_GPMaRj94JKmEO70f6xNfpdoYU5bvv&output=json&search=' + mac;
+	console.log(mac)
+	getData();
+	searchInput.value = '';
+	document.getSelection().selectAllChildren(macNode)
+})
+
+searchInput.addEventListener("keyup", (event) => {
+	event.preventDefault();
+	if (event.keyCode === 13) {
+		searchBtn.click();
+	}
+})
